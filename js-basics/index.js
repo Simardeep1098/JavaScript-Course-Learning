82561c446f942cc5b4ae6879725f3bbf6344563c
Simardeep1098 <Simardeep1098@users.noreply.github.com>
2022-07-21 13:39:28 +1000

    JS learnings

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,2 +1,54 @@
-        //comment
-        console.log('Hello WOrld');
+
+// FOR-IN
+// 
+// const person = {
+//         name: 'Simar',
+//         age: 23 
+// };
+// for (let key in person)
+// console.log(key, person[key]);
+
+// const colors = ['red', 'green', 'blue'];
+// for (let index in colors)
+//         console.log(index, colors[index]);
+
+
+// Break COntinue
+//
+// let i=0;
+// while (i <= 10) {
+//         // if ( i === 5) break;
+//         // console.log(i);
+//         // i++;
+//         if ( i % 2 == 0) {
+//         i++;
+//         continue;
+// }
+//         console.log(i);
+//         i++;
+// }
+
+
+// Exercise:::::::::::
+// Write a function that takes two numbers and returns the maximum of the two. 
+// Both methods shown below are corret. 
+//
+// let number = max(5,10);
+// console.log(number);
+//
+// function max(a,b) {
+//       
+// 1..        if ( a > b) return a;
+//         return b;
+//
+// 2...       return (a > b) ? a : b;
+// }
+
+
+// Exercise::::::::::::
+// function returns True if it's landscare ( i.e. width > height) else return false.
+//
+// console.log(isLandscape(800,600));
+// function isLandscape(width, height) {
+//         return (width > height);
+// }
