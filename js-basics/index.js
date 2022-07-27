1e6c432f447b3d9dc343a60dbf8fa29c6cbaab79
Simardeep1098 <72374937+Simardeep1098@users.noreply.github.com>
2022-07-27 12:56:16 +1000

    JS learnings

--- js-basics/index.js
+++ js-basics/index.js
@@ -51,4 +51,61 @@
 // console.log(isLandscape(800,600));
 // function isLandscape(width, height) {
 //         return (width > height);
+//          }
+
+
+// FizzBuzz funtion::::::::::::
+// const output = FizzBuzz(true);
+// console.log(output);
+// function FizzBuzz(input){
+
+//     if (typeof input !== 'number')
+//     return NaN;
+
+//     if((input % 3 === 0) && (input % 5 === 0))
+//     return 'FizzBuzz';
+
+//     if (input % 3 === 0)
+//     return 'Fizz';
+
+//     if (input % 5 === 0)
+//     return 'Buzz';
+
+//     return input;
 // }
+
+
+// //Check speed , Match.floor Exercise::::::::::
+// checkSpeed(131) ;
+// function checkSpeed(speed) {
+
+//     const speedLimit = 70;
+//     const kmPerPoint = 5;
+    
+//     if (speed < speedLimit + kmPerPoint){
+//         console.log('ok');
+//         return;
+//     }
+//     const points = Math.floor((speed - speedLimit) / kmPerPoint);
+//     if (points >= 12)
+//         console.log('License Suspended');
+//     else
+//         console.log('Points:', points);  
+// }
+
+
+// Even Odd (showNumber) Exercise::::::::::::::
+// showNumbers(10);
+// function showNumbers(limit){
+//     for (let i = 0; i <= limit; i++) {
+
+//  1.   if (i % 2 === 0) console.log(i, 'Even');
+//         else console.log(i, 'Odd');
+    
+//  2.  const message = (i % 2 === 0) ? 'Even' : 'Odd' 
+//         console.log(i, message);
+    
+// }
+// }
+
+
